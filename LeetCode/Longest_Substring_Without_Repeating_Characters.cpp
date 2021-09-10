d57924472985eb4ef1a313f98467f9ea12ceaa11
Nourhan Sowar <48545560+NourhanSowar@users.noreply.github.com>
2021-09-10 16:19:15 +0200

    Get the length of unique substring
    Using sliding Window technique and hashset in C++

---
+++ LeetCode/Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+
+using namespace std; 
+
+
+class Solution {
+    public:
+        int maxs (int x, int y){
+            //return  (x>y) ? x :y;
+        }
+    public :
+        int lengthOfLongestSubstring(string s) {
+
+            int i=0 ;
+            int j=0;
+            int returned_length =0;
+            set<char>hash; // create hashset to store the unique elements.
+
+            while (i<s.length() && j<s.length() ){
+
+                 /* if 2 elemtns in hashset are repeated , then, remove the first repeated elemtent 
+                 and change the start point by increment i */
+                if (hash.count(s[j])){
+                    hash.erase(s[i]);
+                    i=i+1;
+                }
+                /* insert the unique element in hash and increment j and  increment the returned length using maxs function*/
+                else{
+                    hash.insert(s[j]);
+                    j=j+1;
+
+                    returned_length = maxs(j-1,returned_length );
+
+                }
+            }
+            return returned_length;
+        }
+        
+};
+int main (){
+    
+    string mystring;
+    cout << "Enter the string:";
+    cin >> mystring;
+    
+    //cout<< mystring;
+
+    Solution myobj;
+    cout<< myobj.lengthOfLongestSubstring(mystring);
+  
+    
+    return 0;    
+ }
